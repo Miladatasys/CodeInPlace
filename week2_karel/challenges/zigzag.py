e8c37ff517e54f0e145d9ba8e52c423a730408e1
Mila Data <camilamorales.it@gmail.com>
2025-04-28 22:00:44 -0400

    feat(stepwise-refinament): Code working ¡finally!

--- week2_karel/challenges/zigzag.py
+++ week2_karel/challenges/zigzag.py
@@ -6,6 +6,12 @@ such as all odd columns have beepers in row 1 and all even columns
 have beepers in row 2. The world will always have 2 rows and
 an even number of columns. Karel should end up in the bottom of
 the right corner of the world. 
+Steps:
+1. Place beeper
+2. Ascend to top row
+3. move
+4. Descend to bottom row
+5. Place beeper
 """
 
 def main():
@@ -15,27 +21,37 @@ def main():
     Post:   Karel ends up at the bottom-right corner, 
             facing East, with beepers in a zig-zag pattern
     """
+    while front_is_clear():
+        place_beeper_and_ascend()
+        descend_to_bottom()
     
 
     
-def place_beeper_and_move_up():
+def place_beeper_and_ascend():
     """
     Turns North, moves up one row, turns East to continue placing beepers.
     Pre: Karel is facing East at bottom row.
     Post: Karel is facing East at top row.
     """
-    pass
+    put_beeper()
+    turn_left()
+    move()
+    turn_right()
+    move()
 
 
-def return_to_bottom_row():
+def descend_to_bottom():
     """
     Turns South, moves down one row, and turns East to continue.
     Pre: Karel is facing East at top row.
     Post: Karel is facing East at bottom row.
     """
-    pass
-
-
+    put_beeper()
+    turn_right()
+    move()
+    turn_left()
+    if front_is_clear():
+        move()
 
 # Low-level helper functions
 def turn_right():
